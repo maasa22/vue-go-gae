4c9b169ccadd0c037198f194dc1dacecb7e56738
masaki <maa22sa@gmail.com>
2019-11-09 18:27:15 +0900

    vueとgoをGAEでデプロイして一旦API通信ができるようになったもの

---
+++ client-vue/vue.config.js
@@ -0,0 +1,12 @@
+// // vue.config.js
+// module.exports = {
+//   devServer: {
+//     proxy: {
+//       "/api": {
+//         target: "https://test-gcp-ma-20191105.appspot.com", //これは開発用なので本番では使えない。
+//         // target: "http://localhost:8888",
+//         changeOrigin: true
+//       }
+//     }
+//   }
+// };
