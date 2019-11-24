b4fddb3856be4e79dd7d4e30eb272ae41331ba68
masaki <maa22sa@gmail.com>
2019-11-24 18:46:42 +0900

    CRUD with vue & go

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,37 @@
 <template>
   <div id="app">
-    <button @click="get_todos">api button</button>
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <h2>CRUD</h2>
+    <h3>Read all</h3>
+    <button @click="getevents">"GET" /events</button>
+    <h3>Read one</h3>
+    <input type="text" name="idRead" placeholder="id" v-model="idRead" required />
+    <button @click="geteventsId">"GET" /events/{id}</button>
+    <h3>Create one</h3>
+    <input type="text" name="idCreate" placeholder="id" v-model="idCreate" required />
+    <input type="text" name="titleCreate" placeholder="title" v-model="titleCreate" required />
+    <input
+      type="text"
+      name="descriptionCreate"
+      placeholder="description"
+      v-model="descriptionCreate"
+      required
+    />
+    <button @click="addevent">"POST" /events</button>
+    <h3>Update one</h3>
+
+    <input type="text" name="idUpdate" placeholder="id" v-model="idUpdate" required />
+    <input type="text" name="titleUpdate" placeholder="title" v-model="titleUpdate" required />
+    <input
+      type="text"
+      name="descriptionUpdate"
+      placeholder="description"
+      v-model="descriptionUpdate"
+      required
+    />
+    <button @click="updateevent">"PATCH" /events</button>
+    <h3>Delete one</h3>
+    <input type="text" name="idDelete" placeholder="id" v-model="idDelete" required />
+    <button @click="removeAtricleId">"DELETE" /events</button>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
@@ -17,18 +47,47 @@ export default {
   },
   data() {
     return {
-      todos: []
+      events: [],
+      api_url: "http://localhost:8090",
+      //   "https://api-dot-vue-go-gcp-20191111.appspot.com"
+      idRead: "",
+      idCreate: "",
+      titleCreate: "",
+      descriptionCreate: "",
+      idDelete: "",
+      idUpdate: "",
+      titleUpdate: "",
+      descriptionUpdate: ""
     };
   },
   methods: {
-    get_todos: async function() {
-      // let res = await axios.get("https://jsonplaceholder.typicode.com/todos/1"); // dummy api on net
-      let res = await axios.get("http://localhost:8090/api/todos"); // local go server
-      // let res = await axios.get(
-      //   "https://api-dot-vue-go-gcp-20191111.appspot.com"
-      // ); // server on gae
-      this.todos = res.data;
-      console.log(this.todos);
+    getevents: async function() {
+      let res = await axios.get(this.api_url + "/events");
+      console.log(res.data);
+    },
+    geteventsId: async function() {
+      let res = await axios.get(this.api_url + "/events/" + this.idRead);
+      console.log(res.data);
+    },
+    addevent: async function() {
+      let res = await axios.post(this.api_url + "/event", {
+        id: this.idCreate,
+        title: this.titleCreate,
+        description: this.descriptionCreate
+      });
+      console.log(res);
+    },
+    updateevent: async function() {
+      let res = await axios.patch(this.api_url + "/events/" + this.idUpdate, {
+        id: this.idUpdate,
+        title: this.titleUpdate,
+        description: this.descriptionUpdate
+      });
+      console.log(res);
+    },
+    removeAtricleId: async function() {
+      let res = await axios.delete(this.api_url + "/events/" + this.idDelete);
+      console.log(res);
     }
   }
 };
--- server/main.go
+++ server/main.go
@@ -1,81 +1,131 @@
+
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
+	"github.com/gorilla/handlers"
+
+	"github.com/gorilla/mux"
 )
 
-// Todo ... This is a todo
-type Todo struct {
-	id       int
-	todo     string
-	priority string
+type event struct {
+	ID          string `json:"ID"`
+	Title       string `json:"Title"`
+	Description string `json:"Description"`
 }
 
-// 初期化
-var initialTodos []Todo
+type allEvents []event
+
+var events = allEvents{
+	{
+		ID:          "1",
+		Title:       "Introduction to Golang",
+		Description: "Come join us for a chance to learn how golang works and get to eventually try it out",
+	},
+	{
+		ID:          "2",
+		Title:       "222",
+		Description: "22222222",
+	},
+	{
+		ID:          "3",
+		Title:       "33",
+		Description: "333",
+	},
+	{
+		ID:          "4",
+		Title:       "44",
+		Description: "444",
+	},
+}
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
+
+func createEvent(w http.ResponseWriter, r *http.Request) {
+	var newEvent event
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+	
+	json.Unmarshal(reqBody, &newEvent)
+	events = append(events, newEvent)
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newEvent)
+}
+
+func getOneEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+
+	for _, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
+}
+
+func getAllEvents(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(events)
+}
+
+func updateEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+	var updatedEvent event
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+	json.Unmarshal(reqBody, &updatedEvent)
+
+	for i, singleEvent := range events {
+		fmt.Println(i, singleEvent)
+		if singleEvent.ID == eventID {
+			singleEvent.Title = updatedEvent.Title
+			singleEvent.Description = updatedEvent.Description
+			var eventTmp []event
+			eventTmp = events[i+1:]
+			events = append(events[:i], singleEvent)
+			events = append(events, eventTmp...)
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
+}
+
+func deleteEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			events = append(events[:i], events[i+1:]...)
+			fmt.Fprintf(w, "The event with ID %v has been deleted successfully", eventID)
+		}
+	}
+}
 
 func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/event", createEvent).Methods("POST")
+	router.HandleFunc("/events", getAllEvents).Methods("GET")
+	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
+	router.HandleFunc("/events/{id}", updateEvent).Methods("PATCH")
+	router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
 
-	initialTodos = append(initialTodos,
-		Todo{
-			id:       1,
-			todo:     "Consider Adam as a candidate",
-			priority: "life changing",
-		},
-		Todo{
-			id:       2,
-			todo:     "Take out the garbage",
-			priority: "life changing",
-		},
-		Todo{
-			id:       3,
-			todo:     "Paint the house",
-			priority: "important",
-		},
-	)
 
-	http.HandleFunc("/", indexHandler)
+	// Where ORIGIN_ALLOWED is like `scheme://dns[:port]`, or `*` (insecure)
+	headersOk := handlers.AllowedHeaders([]string{"content-type"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PATCH"})
 
-	// [START setting_port]
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8090"
-		log.Printf("Defaulting to port %s", port)
-	}
-
-	log.Printf("Listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
-	// [END setting_port]
-}
-
-// indexHandler responds to requests with our greeting.
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*") //本来はallowするURLを指定とかすべきそう。
-	// if r.URL.Path != "/" {
-	// 	http.NotFound(w, r)
-	// 	return
-	// }
-	// fmt.Fprint(w, "Hello, World!")
-
-	if r.URL.Path == "/api" { //なぜか、こいつだけ動かない。直下はダメ？
-		fmt.Fprint(w, "api")
-	} else if r.URL.Path == "/api/hoge" { //URL上での最後のスラッシュ、goでの最後のスラッシュ、vueでの最後のスラッシュの有無で動いたり動かなかったりする。
-		fmt.Fprint(w, "api/hogeee")
-	} else if r.URL.Path == "/api/fuga" {
-		fmt.Fprint(w, "api/fuga")
-	} else if r.URL.Path == "/api/todos" {
-		for i, s := range initialTodos {
-			textTmp := ""
-			textTmp = fmt.Sprintf("index: %d, name: %s\n", i, s.todo)
-			fmt.Fprint(w, textTmp)
-		}
-		// fmt.Fprint(w, initialTodos[0])
-		// fmt.Fprint(w, initialTodos[0].id)
-	}
-
+	// start server listen
+	// with error handling
+	log.Fatal(http.ListenAndServe(":8090", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
 }
